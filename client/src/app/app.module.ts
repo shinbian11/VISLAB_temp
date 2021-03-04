67ba0a1104b8c1e4339303ab9d1394cda2f33556
hjsong <thrubits@gmail.com>
2021-03-04 09:50:40 +0900

    + initial commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoutingModule } from './routing/routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavListComponent } from './components/sidenav-list/sidenav-list.component';
+import { MemberComponent } from './components/member/member.component';
+import { PublicationComponent } from './components/publication/publication.component';
+import { ResearchComponent } from './components/research/research.component';
+import { CourseComponent } from './components/course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LayoutComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    MemberComponent,
+    PublicationComponent,
+    ResearchComponent,
+    CourseComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RoutingModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatChipsModule,
+    MatGridListModule,
+    MatListModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/routing/routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import {MemberComponent} from '../components/member/member.component';
+import {PublicationComponent} from '../components/publication/publication.component';
+import {ResearchComponent} from '../components/research/research.component';
+import {CourseComponent} from '../components/course/course.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'members', component: MemberComponent },
+  { path: 'research', component: ResearchComponent },
+  { path: 'publications', component: PublicationComponent },
+  { path: 'courses', component: CourseComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class RoutingModule { }
