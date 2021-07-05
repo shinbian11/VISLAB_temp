004192a08d02220b88930fcc01393e737f747b2f
shinbian11 <shinbian11@gmail.com>
2021-07-05 10:16:44 +0900

    commit

--- client/src/app/components/research-list/research-list.ts
+++ client/src/app/components/research-list/research-list.ts
@@ -5,5 +5,5 @@ export const ResearchList : Research[] = [
  { title : 'VISLAB', obliqueDesc : 'Developing VISLAB Website. Using Angular, Node.js, express, PostgreSQL.', members : 'Ji Hwan Kim and Jung Min Lee / 2021.6 ~', image : 'assets/images/main_banner_0.jpg'},
  { title : 'CareNet', obliqueDesc : 'A project to analyze and visualize the power data of Korea Electric Power Corporation (KEPCO)', members : 'Sang Jun Park and San Hong / 2021.6 ~', image : 'assets/images/CareNet_main.jpg'},
  { title : 'PatientFlow', obliqueDesc : 'A project that analyzes and visualizes hospital-related data across the country and clusters it', members : 'Jung Yeon Kim and Jin Woo Park / 2021.6 ~', image : 'assets/images/PatientFlow_main.png'},
- 
+
 ];
