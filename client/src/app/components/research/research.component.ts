0f9cdc6cbd81fcc0a293169a2530663a006a75ae
shinbian11 <shinbian11@gmail.com>
2021-07-05 13:45:31 +0900

    commit

--- client/src/app/components/research/research.component.ts
+++ client/src/app/components/research/research.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-import { rawListeners } from 'node:process';
+//import { rawListeners } from 'node:process';
 import { Research } from "./research";
 import { ResearchList } from './research-list';
 
@@ -16,7 +16,7 @@ export class ResearchComponent implements OnInit {
   // List( Set(researchTotal.map(r => r.year)) ).map(element => {
   //   this.researchTotal.filter(r => r.year === element)
   // });
-  
+
   research2021 = this.researchTotal.slice(0, 3);
   research2020 = this.researchTotal.slice(3, 5);
   research2019 = this.researchTotal.slice(5, 9);
