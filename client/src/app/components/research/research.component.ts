2d0038a70b941a779fc71ea67287d3f960168df3
shinbian11 <shinbian11@gmail.com>
2021-07-05 16:12:43 +0900

    7.5- 피드백 이후 component ngFor 문으로 간결화 성공!

--- client/src/app/components/research/research.component.ts
+++ client/src/app/components/research/research.component.ts
@@ -13,14 +13,18 @@ export class ResearchComponent implements OnInit {
 
   researchTotal : Research[] = ResearchList;
 
-  // List( Set(researchTotal.map(r => r.year)) ).map(element => {
+  // List(new Set(this.researchTotal.map(r => r.year))).map(element => {
   //   this.researchTotal.filter(r => r.year === element)
   // });
 
-  research2021 = this.researchTotal.slice(0, 3);
-  research2020 = this.researchTotal.slice(3, 5);
-  research2019 = this.researchTotal.slice(5, 9);
-  research2018 = this.researchTotal.slice(9, 11);
+  
+  year = [2021,2020,2019,2018];
+  researchYear = this.year.map(element => this.researchTotal.filter(r => r.year === element));
+
+  // research2021 = this.researchTotal.slice(0, 3);
+  // research2020 = this.researchTotal.slice(3, 5);
+  // research2019 = this.researchTotal.slice(5, 9);
+  // research2018 = this.researchTotal.slice(9, 11);
 
   constructor() { }
 
