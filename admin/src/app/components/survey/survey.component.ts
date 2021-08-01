87cd31b5bbb6ab352c33ea06afb0ab3f9577f005
shinbian11 <shinbian11@gmail.com>
2021-08-01 16:55:38 +0900

    8.1 commit - page에 따라 survey .html 파일 내용 다르게 구성 완료!

--- admin/src/app/components/survey/survey.component.ts
+++ admin/src/app/components/survey/survey.component.ts
@@ -1,14 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Subject, Observable } from 'rxjs';
-import { MemberService } from 'src/app/services/member.service';
-import { Member } from 'src/app/models/member';
-import { PublicationService } from 'src/app/services/publication.service';
-import { Publication } from 'src/app/models/publication';
-import { ResearchService } from 'src/app/services/research.service';
-import { Research } from 'src/app/models/research'; 
-import { CourseService } from 'src/app/services/course.service';
-import { Course } from 'src/app/models/course';
 
 @Component({
   selector: 'app-survey',
@@ -20,56 +12,39 @@ export class SurveyComponent implements OnInit {
 
   //member.component.ts에서 modalRef.componentInstance.renderPage = page; 를 통해 전달된 renderPage 변수를 받아옴!
   //renderPage 변수에는 (member/publication/research/course/event/banner) 중 하나가 들어있다.
-  //이 변수의 값에 따라서, Survey 컴포넌트의 html 파일을 조금씩 다르게 보여줘야 한다.
+  //이 변수의 값에 따라서, Survey 컴포넌트의 html 파일을 조금씩 다르게 보여주는 역할을 ngOnInit() 내에 추가함.
 
  @Input() public renderPage;
 
   array1 : string[] = ['member','publication','research','course','event','banner'];
-  answer;
-  ren;
+  answerFields : string[][] =[
+    ['index', 'name', 'role', 'degree', 'employment', 'email', 'is_alumni', 'image_path', 'actions'],
+    ['id','title','venue','type','conference_prefix','published_in','published_url','volume','no','pages','year','conference_info','postfix','status','rank'],
+    [ 'id','title','year','month','obliquedesc','members'],
+    ['id','type','title','subtitle','content','subcontent','prerequisite','semester_button']
+  ]
 
-  member : Member[]=[];
-  publication :Publication[]=[];
-  research : Research[]=[];
-  course : Course[]=[];
-  
-  constructor(
-    public modal: NgbActiveModal,
-    private ms: MemberService, 
-    private ps: PublicationService, 
-    private rs: ResearchService, 
-    private cs: CourseService) { 
+  renderingRow : string[];
+  renderingField : string;
 
+  constructor(public modal: NgbActiveModal) {
     this.renderPage = this.renderPage;
   }
 
 
   ngOnInit(): void {
 
-    this.ms.getAll().subscribe(member => {
-      this.member = member.sort((a, b) => a.index - b.index);
-    });
-    this.ps.getAll().subscribe(publication => {
-      this.publication = publication.sort((a, b) => a.id - b.id);
-    });
-    this.rs.getAll().subscribe(research => {
-      this.research = research.sort((a, b) => a.id - b.id);
-    });
-    this.cs.getAll().subscribe(course => {
-      this.course = course.sort((a, b) => a.id - b.id);
-    });
-
-    this.answer = [this.member,this.publication,this.research,this.course]; // 여기에서 값이 안 들어가네 ㅠㅠ
+    //html 파일 form 부분 다르게 구성하는 부분
     
-    //this.answer1.forEach((element,idx)=>console.log(idx+ '||'+ element));
-
     this.array1.forEach(
       (element, idx) => {
         if(element === this.renderPage){
-          this.ren = this.answer[idx];
+         this.renderingRow = this.answerFields[idx];
+         this.renderingField = element;
         }
       }
     );
+
   }
   
 }
