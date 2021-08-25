faa7a2d4546559fa630437a020f003df34833f6f
shinbian11 <shinbian11@gmail.com>
2021-08-25 16:10:59 +0900

    8.25) - News page 구현 완료. 나중에 각각의 뉴스에 대해 page 구축하면 router 로 연결하기!

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { INews } from 'src/app/shared/models/news';
+import { NewsService } from 'src/app/shared/services/news.service';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
- 
+  constructor(private ns: NewsService) { }
+  
+  data : Date;
+  News : INews[]=[];
+
   ngOnInit(): void {
-   
+    this.ns.getAll().subscribe(News => {
+
+      //최근 뉴스일수록 추가하면 id 값은 커지니까 (내림차순 정렬)
+       News = News.sort((a, b) => b.id - a.id); 
+
+       //최근 뉴스 3개만 보여주기
+       this.News[0] = News[0];
+       this.News[1] = News[1];
+       this.News[2] = News[2];
+
+      //e.g.) (날짜 형식 변환) 2021-08-21T15:00:00.000Z 의 Date 형식을 2021-08-21 string 형식으로 변환
+       this.News.forEach(element => element.date = this.ChangeDate(element.date)) 
+
+    });
+
   }
   
+  ChangeDate(date) : string {
+    this.data = new Date(date)
+    //console.log(this.data.getFullYear()+'-' + (this.data.getMonth()+1) + '-'+this.data.getDate());
+    return this.data.getFullYear()+'-' + (this.data.getMonth()+1) + '-'+this.data.getDate();
+  }
 
 }
+
--- client/src/app/components/news/news.component.ts
+++ client/src/app/components/news/news.component.ts
@@ -14,12 +14,23 @@ export class NewsComponent implements OnInit {
   constructor(private ns: NewsService) { }
 
   News : INews[]=[];
+  data : Date;
+  monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   ngOnInit(): void {
     this.ns.getAll().subscribe(News => {
-       this.News = News.sort((a, b) => a.id - b.id);
-       
+       this.News = News.sort((a, b) => b.id - a.id);
+
+       //e.g.) (날짜 형식 변환) 2021-08-21T15:00:00.000Z 의 Date 형식을 2021-08-21 string 형식으로 변환
+       this.News.forEach(element => element.date = this.ChangeDate(element.date)) 
        });
       }
 
+  ChangeDate(date) : string {
+      this.data = new Date(date);
+
+      //console.log(this.data.getFullYear()+'-' + (this.data.getMonth()+1) + '-'+this.data.getDate());
+      return (this.monthNames[this.data.getMonth()] + ' ' + this.data.getDate() + ', ' + this.data.getFullYear());
+      
+    }
 }
